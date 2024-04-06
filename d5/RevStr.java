9164ff514fa7eb7bc6cc3712238b3c3d1bc58277
blazingmoons <67993058+blazingmoons@users.noreply.github.com>
2024-04-06 18:02:21 +0530

    RevStr.java

---
+++ d5/RevStr.java
@@ -0,0 +1,51 @@
+// Java program to reverse a string
+import java.util.*;
+class  RevStr{
+
+	
+	static void reverse(char str[], int start, int end)
+	{
+
+		char temp;
+
+		while (start <= end) {
+			
+			temp = str[start];
+			str[start] = str[end];
+			str[end] = temp;
+			start++;
+			end--;
+		}
+	}
+	
+	static char[] reverseWords(char[] s)
+	{
+		
+		int start = 0;
+		for (int end = 0; end < s.length; end++) {
+			if (s[end] == ' ') {
+				reverse(s, start, end);
+				start = end + 1;
+			}
+		}
+
+		//  last word
+		reverse(s, start, s.length - 1);
+
+		// Reverse the entire String
+		reverse(s, 0, s.length - 1);
+		return s;
+	}
+
+	//
+	public static void main(String[] args)
+	{
+		String s = "i like this program very much ";
+
+	
+		char[] p = reverseWords(s.toCharArray());
+		System.out.print(p);
+	}
+}
+
+
